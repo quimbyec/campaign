925637eb792b06e31d609e838158f4746ce973ea
Emily Chafin <quimbyec@gmail.com>
2024-07-19 22:07:24 -0400

    Quartz sync: Jul 19, 2024, 10:07 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,14 +8,14 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "🪴 Emily's Campaign",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "quimbyec.github.io/campaign",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
--- templates/Private.md
+++ templates/Private.md
@@ -1,5 +1,6 @@
 ---
 title: 
 draft: true
-tags:
+tags: 
+date:
 ---
